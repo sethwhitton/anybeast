571b685055e903896905acfed1fef7165838a4d4
Seth Whitton <sethwhitton@swingingkazoo.local>
2016-05-30 11:10:59 -0400

    Partial fixes to convert the absolute/fixed frame to a separate design solution

--- public/javascript/frame.js
+++ public/javascript/frame.js
@@ -1,64 +1,64 @@
 
 $(document).ready(function(){
 
-  var $elBegin = $('#about'),
-      $elEnd = $('.our-disciplines'),
-      $frameWrap = $('.frame-wrap'),
-      scrollBegin = $elBegin.offset().top,
-      scrollEnd = $elEnd.offset().top,
-      scrollTop,
-      winHeight,
-      mobileHeight,
-      tabletHeight,
-      desktopHeight;
-
-
-  function calculateValues() {
-    scrollTop = $(window).scrollTop();
-    winHeight = window.innerHeight;
-    desktopHeight = $('.desktop').outerHeight(true);
-  }
-
-  function framePadding() {
-    // if ($(window).width() > 768){
-      $frameWrap.removeAttr('style')
-      $frameWrap.css('padding-top', (winHeight - desktopHeight) / 2 + 'px');
-    // }
-  }
-
-  function positionFrame() {
-    if (scrollTop >= scrollBegin) {
-      $frameWrap.addClass('active');
-      }
-    if (scrollTop < scrollBegin) {
-      $frameWrap.removeClass('active');
-      }
-    if (scrollTop >= scrollEnd) {
-      $frameWrap.removeClass('active');
-      $frameWrap.addClass('end-active');
-      // $frameWrap.css('padding-top', '');
-      }
-    if (scrollTop < scrollEnd) {
-      $frameWrap.removeClass('end-active');
-      }
-    }
-
-    $('.desktop').load(function(){
-      calculateValues();
-      framePadding();
-    });
-
-    $(window).on("resize",function() {
-      calculateValues();
-      framePadding();
-      positionFrame();
-    });
-
-    $(window).on("scroll",function() {
-      calculateValues();
-      // framePadding();
-      positionFrame();
-    });
+  // var $elBegin = $('#about'),
+  //     $elEnd = $('.our-disciplines'),
+  //     $frameWrap = $('.frame-wrap'),
+  //     scrollBegin = $elBegin.offset().top,
+  //     scrollEnd = $elEnd.offset().top,
+  //     scrollTop,
+  //     winHeight,
+  //     mobileHeight,
+  //     tabletHeight,
+  //     desktopHeight;
+  //
+  //
+  // function calculateValues() {
+  //   scrollTop = $(window).scrollTop();
+  //   winHeight = window.innerHeight;
+  //   desktopHeight = $('.desktop').outerHeight(true);
+  // }
+  //
+  // function framePadding() {
+  //   // if ($(window).width() > 768){
+  //     $frameWrap.removeAttr('style')
+  //     $frameWrap.css('padding-top', (winHeight - desktopHeight) / 2 + 'px');
+  //   // }
+  // }
+  //
+  // function positionFrame() {
+  //   if (scrollTop >= scrollBegin) {
+  //     $frameWrap.addClass('active');
+  //     }
+  //   if (scrollTop < scrollBegin) {
+  //     $frameWrap.removeClass('active');
+  //     }
+  //   if (scrollTop >= scrollEnd) {
+  //     $frameWrap.removeClass('active');
+  //     $frameWrap.addClass('end-active');
+  //     // $frameWrap.css('padding-top', '');
+  //     }
+  //   if (scrollTop < scrollEnd) {
+  //     $frameWrap.removeClass('end-active');
+  //     }
+  //   }
+  //
+  //   $('.desktop').load(function(){
+  //     calculateValues();
+  //     framePadding();
+  //   });
+  //
+  //   $(window).on("resize",function() {
+  //     calculateValues();
+  //     framePadding();
+  //     positionFrame();
+  //   });
+  //
+  //   $(window).on("scroll",function() {
+  //     calculateValues();
+  //     // framePadding();
+  //     positionFrame();
+  //   });
 
     $('#broadside > .border').click(function() {
       $('.box1').toggleClass('active','modal');
